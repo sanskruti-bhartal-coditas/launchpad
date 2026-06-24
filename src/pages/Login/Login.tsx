0c80c3f7a3861bf3ee75e03685597568b58cb60e
sanskruti.bhartal@coditas <sanskruti.bhartal@coditas.com>
2026-06-24 12:04:22 +0530

    setting up routes

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,11 @@
+import styles from "./Login.module.scss";
+
+const Login = () =>{
+  return(
+    <section className={styles.login}>
+      this is login
+    </section>
+  )
+}
+
+export default Login;
