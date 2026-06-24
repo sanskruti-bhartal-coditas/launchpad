dd4ce6b8ceb0907ff61adb3d83d35eb4ec924abf
sanskruti.bhartal@coditas <sanskruti.bhartal@coditas.com>
2026-06-24 14:07:23 +0530

    made reusable button components, designed UI for login page

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,9 +1,44 @@
+import { PrimaryButton } from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
 import styles from "./Login.module.scss";
 
-const Login = () =>{
-  return(
-    <section className={styles.login}>
-      this is login
+const Login = () => {
+  return (
+    <section className={styles.background}>
+      <div className={styles.container}>
+
+        {/* heading  */}
+        <div className={styles.heading}>
+          <h1>Log in</h1>
+        </div>
+
+        {/* input fields */}
+        <div className={styles.inputGroup}>
+          <Input
+            type="email"
+            placeholder="Enter email..."
+          />
+          {/* error messages */}
+          <div className={styles.errorMessage}>
+
+          </div>
+        </div>
+
+        <div className={styles.inputGroup}>
+          <Input
+            type="password"
+            placeholder="Enter password..."
+          />
+          {/* error messages  */}
+          <div>
+
+          </div>
+        </div>
+
+        <div className={styles.inputGroup}>
+          <PrimaryButton>Login</PrimaryButton>
+        </div>
+      </div>
     </section>
   )
 }
